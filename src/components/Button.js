78bba5a0f116b539a4a4deedcc25135ab55d5c39
cammac <camm@rocktechcomputer.com>
2019-10-31 14:10:52 -0700

    completed adding context

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,15 +7,15 @@ class Button extends React.Component {
     // used with this.context, not Consumer component
     // static contextType = LanguageContext
 
-    renderSubmit(value) {
-        return value === 'english' ? 'Submit' : 'Voorleggen'
+    renderSubmit(language) {
+        return language === 'english' ? 'Submit' : 'Voorleggen'
     }
 
     renderButton(color) {
         return (
             <button className={`ui button ${color}`}>
                 <LanguageContext.Consumer>
-                    {(value) => this.renderSubmit(value)}
+                    {({ language }) => this.renderSubmit(language)}
                 </LanguageContext.Consumer>
             </button>
         )
